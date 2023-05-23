7e24ae8f5894594eb5adc48abe9600af8329facc
krishna dupatane <72943224+dupatane@users.noreply.github.com>
2023-05-24 05:17:25 +0530

    Update fleetapp.groovy

--- vars/fleetapp.groovy
+++ vars/fleetapp.groovy
@@ -1,5 +1,6 @@
-def call(String registryCred = 'a', String registryin = 'a', String docTag = 'a', String grepo = 'a', String gbranch = 'a', String gitcred = 'a') {
+@Library("fleetapp")_
 
+def call(String registryCred = 'a', String registryin = 'a', String docTag = 'a', String grepo = 'a', String gbranch = 'a', String gitcred = 'a') {
     pipeline {
         agent any
 
@@ -11,17 +12,19 @@ def call(String registryCred = 'a', String registryin = 'a', String docTag = 'a'
             } 
             stage('BUILD IMAGE') {
                 steps {
-                    sh 'cd "/var/lib/jenkins/workspace/fleetapp/sourcecode/k8s-fleetman-api-gateway" && docker build -t ${registryin}:${docTag} .'           
+                    sh "cd \"/var/lib/jenkins/workspace/fleetapp/sourcecode/k8s-fleetman-api-gateway\" && docker build -t ${registryin}:${docTag} ."
                 }
             }
             stage('PUSH HUB') {
                 steps {
                     withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]){
-                        sh 'docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}'
-                        sh 'docker push ${registryin}:${docTag}'                   	
+                        sh "docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}"
+                        sh "docker push ${registryin}:${docTag}"                   	
                     }    
                 }
             } 
         }
     }  
 }
+
+call('a', 'dupatane/docker', 'fleet', 'https://github.com/dupatane/kubernetes-fleetapp-project.git', 'main', 'a')
