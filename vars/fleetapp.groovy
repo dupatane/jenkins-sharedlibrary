8872709dbfe676bc8c9c82c72965b49e9cccb0b8
krishna dupatane <72943224+dupatane@users.noreply.github.com>
2023-05-24 05:03:19 +0530

    Update fleetapp.groovy

--- vars/fleetapp.groovy
+++ vars/fleetapp.groovy
@@ -1,36 +1,27 @@
 def call(String registryCred = 'a', String registryin = 'a', String docTag = 'a', String grepo = 'a', String gbranch = 'a', String gitcred = 'a') {
 
-pipeline {
-environment { 
-		registryCredential = "${registryCred}"
-		registry = "$registryin" 	
-		dockerTag = "${docTag}$BUILD_NUMBER"
-		gitRepo = "${grepo}"
-		gitBranch = "${gbranch}"
-		gitCredId = "${gitcred}"
-	}
-		
-    agent any
+    pipeline {
+        agent any
 
-    stages {
-        stage("POLL SCM"){
-            	steps {
-                	checkout([$class: 'GitSCM', branches: [[name: "$gitBranch"]], extensions: [], userRemoteConfigs: [[credentialsId: "$gitCredId", url: "$gitRepo"]]])             
-            	}
-        } 
-        stage('BUILD IMAGE') {
-            	steps {
-			sh 'cd "/var/lib/jenkins/workspace/fleetapp/sourcecode/k8s-fleetman-api-gateway" && docker build -t $registry:$dockerTag .'           
-            	}
+        stages {
+            stage("POLL SCM"){
+                steps {
+                    checkout([$class: 'GitSCM', branches: [[name: gbranch]], extensions: [], userRemoteConfigs: [[credentialsId: gitcred, url: grepo]]])             
+                }
+            } 
+            stage('BUILD IMAGE') {
+                steps {
+                    sh 'cd "/var/lib/jenkins/workspace/fleetapp/sourcecode/k8s-fleetman-api-gateway" && docker build -t $registryin:$docTag .'           
+                }
+            }
+            stage('PUSH HUB') { 
+                steps {
+                    withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]){
+                        sh 'docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD'
+                        sh 'docker push $registryin:$docTag'                   	
+                    }    
+                }
+            } 
         }
-        stage('PUSH HUB') { 
-            	steps {
-			withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]){
-			sh 'docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD'
-			sh 'docker push $registry:$dockerTag'                   	
-			}    
-		}
-        } 
-    }
-}  
+    }  
 }
