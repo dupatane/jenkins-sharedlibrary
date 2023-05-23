bc8e855dca38d78ae274e66a0cf5242756781e8a
krishna dupatane <72943224+dupatane@users.noreply.github.com>
2023-05-24 05:10:05 +0530

    Update fleetapp.groovy

--- vars/fleetapp.groovy
+++ vars/fleetapp.groovy
@@ -4,21 +4,21 @@ def call(String registryCred = 'a', String registryin = 'a', String docTag = 'a'
         agent any
 
         stages {
-            stage("POLL SCM"){
+            stage("checkout") {
                 steps {
-                    checkout([$class: 'GitSCM', branches: [[name: gbranch]], extensions: [], userRemoteConfigs: [[credentialsId: gitcred, url: grepo]]])             
+                    checkout([$class: 'GitSCM', branches: [[name: "${gbranch}"]], extensions: [], userRemoteConfigs: [[credentialsId: "${gitcred}", url: "${grepo}"]]])
                 }
             } 
             stage('BUILD IMAGE') {
                 steps {
-                    sh 'cd "/var/lib/jenkins/workspace/fleetapp/sourcecode/k8s-fleetman-api-gateway" && docker build -t $registryin:$docTag .'           
+                    sh 'cd "/var/lib/jenkins/workspace/fleetapp/sourcecode/k8s-fleetman-api-gateway" && docker build -t ${registryin}:${docTag} .'           
                 }
             }
-            stage('PUSH HUB') { 
+            stage('PUSH HUB') {
                 steps {
                     withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]){
-                        sh 'docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD'
-                        sh 'docker push $registryin:$docTag'                   	
+                        sh 'docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}'
+                        sh 'docker push ${registryin}:${docTag}'                   	
                     }    
                 }
             } 
